7dd32644923a7476157038266bab90100bb87cc8
tihitna sisay <tihitna222sisay@gmail.com>
2022-07-11 20:21:06 +0300

    modify for es6

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,23 +1,23 @@
 import $ from 'jquery';
 import _ from 'lodash';
-var count = 0
+const count = 0
 
 
 
-var newPar = $('body').append('<p>Holberton Dashboard</p>');
-$(newPar ).append('<p>Dashboard data for the students</p>');
-$(newPar).append('<button>Dashboard data for the students</button>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Dashboard data for the students</button>');
 
-$(newPar).append("<p id='count'></p>");
+$('body').append("<p id='count'></p>");
 
-$(newPar).append('<p>Copyright - Holberton School</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 
 
 function updateCounter(){
    
     count +=1
-    $("#count").html(`${count} clicks on the button`)
+    $("#count").text(`${count} clicks on the button`)
 }
 
 $('button').on('click', _.debounce(updateCounter, 500));
