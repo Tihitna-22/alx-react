53c9933a93c3ebce4fc7c95ae84b327ed9785e2e
tihitna sisay <tihitna222sisay@gmail.com>
2022-07-11 12:33:23 +0300

    add task_0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from "jquery"
+
+var newPar = $('body').append("<p>Holberton Dashboard</p>");  
+$( newPar).append("<p>Dashboard data for the students</p>");
+$(newPar).append("<p>Copyright - Holberton School</p>");
+
