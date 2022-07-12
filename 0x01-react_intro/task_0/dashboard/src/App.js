5e31392c2c3ec813813ec302f885a14aee3c4886
tihitna sisay <tihitna222sisay@gmail.com>
2022-07-12 23:30:49 +0300

    add first task

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,30 @@
+import logo from './175b85183ecedb529e14.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className="App-header">
+        <img src={logo} alt="">
+        </img>
+        <hl>
+          School dashboard
+        </hl>
+      </header>
+      <body class="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+      </body>
+      <footer className='App-footer'>
+        <p>
+          Copyright 2020 - holberton School
+        </p>
+      </footer>
+
+    </div>
+
+  );
+}
+
+export default App;
