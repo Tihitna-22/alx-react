c2dbcd44eaa0f2735cc4c4ab86c4692d156d9613
tihitna sisay <tihitna222sisay@gmail.com>
2022-07-21 01:04:11 +0300

    modify app

--- 0x03-React_component/task_0/dashbord/src/App/App.js
+++ 0x03-React_component/task_0/dashbord/src/App/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import './App.css';
-import Login from '../Login/Login'
+import Notifications from '../Notifications/Notifications'
 import Header from '../Header/Header'
+import Login from '../Login/Login'
+import CourseList from '../CourseList/CourseList'
 import Footer from '../Footer/Footer'
-import Notifications from '../Notifications/Notifications';
-import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils'
 
 const listCourses = [
   { id: 1, name: 'ES6', credit: 60 },
   { id: 2, name: 'Webpack', credit: 20 },
   { id: 3, name: 'React', credit: 40 }
 ];
+
 const listNotifications = [
   { id: 1, type: 'default', value: 'New course available' },
   { id: 2, type: 'urgent', value: 'New resume available' },
-  {  id: 3, type: 'urgent', html: { __html: getLatestNotification()}  }
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
 ];
 
-class App extends React.component {
+class App extends React.Component {
   render() {
-
     return (
       <React.Fragment>
         <Notifications listNotifications={listNotifications}></Notifications>
@@ -31,7 +32,6 @@ class App extends React.component {
       </React.Fragment>
     );
   }
-
 }
 
 App.propTypes = {
