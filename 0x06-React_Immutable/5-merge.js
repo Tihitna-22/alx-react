208574d7f6b453fd269b6bb6c0015e0369895dfc
tihitna sisay <tihitna222sisay@gmail.com>
2022-08-03 23:59:05 +0300

    merge and concatinate

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,18 +1,15 @@
 import { List, Map, merge } from 'immutable';
 
-function concatElements(page1, page2) {
-  const page1immut = List(page1);
-  const page2immut = List(page2);
-  const all = page1immut.concat(page2immut);
-  return all;
+export function concatElements(page1, page2) {
+    const page1immut = List(page1);
+    const page2immut = List(page2);
+    const all = page1immut.concat(page2immut);
+    return all;
 }
-function mergeElements(page1, page2) {
-  if (page1 === page2) {
+export function mergeElements(page1, page2) {
+
+    const page1immut = Map(page1);
     const page2immut = Map(page2);
-    return page2immut;
-  }
-  const page1immut = Map(page1);
-  const page2immut = Map(page2);
-  const all = merge(page1immut, page2immut);
-  return all;
+    const all = page1immut.merge(page2immut);
+    return all;
 }
